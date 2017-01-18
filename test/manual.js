38a589ac803ba72695035087e9220c0e7220ed62
Alex Corvi <alex@arrayy.com>
2017-01-18 04:44:15 +0300

    mocha test

--- test/manual.js
+++
@@ -1,3 +0,0 @@
-const cases = require("./manual_cases.js");
-const visualize = require('./visualize.js');
-cases.forEach((sample,index)=>visualize(sample,index));
---
+++ test/test.js
@@ -0,0 +1,32 @@
+const cases = require("./manual_cases.js");
+const visualize = require('./visualize.js');
+const parser = require("../lib/index.js");
+const pos = require("en-pos");
+
+describe('One root for all sentences', function () {
+	cases.forEach((sample,index)=>{
+		it('',function () {
+			this.test.title = sample[0].join(" ");
+			var tags = pos(sample[0]).tags;
+			var parsed = parser(tags);
+			if(parsed.length>1) {
+				visualize(sample[0],index,true);
+				throw new Error("Sentence parsing gave two roots");
+			}
+		});
+	});
+});
+
+describe('Root is correct:', function () {
+	cases.forEach((sample,index)=>{
+		it('',function () {
+			var tags = pos(sample[0]).tags;
+			var parsed = parser(tags);
+			this.test.title = sample[0].join(" ") + ` (${sample[1]}===${parsed[0].index})`;
+			if(parsed[0].index[0]>sample[1] || parsed[0].index[1]<sample[1]) {
+				visualize(sample[0],index,true);
+				throw new Error(`Expected: ${sample[1]}, Result: ${parsed[0].index}`);
+			}
+		});
+	});
+});
