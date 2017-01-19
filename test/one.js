1bd2fd7a027d71de2e2e3b183bb029bba54658fa
Alex Corvi <alex@arrayy.com>
2017-01-19 04:12:07 +0300

    call one test by line number

--- test/one.js
+++ test/one.js
@@ -1,5 +1,5 @@
 const visualize = require("./visualize.js");
 const cases = require('./manual_cases.js');
-const index = process.argv[2] || 0;
-const target = cases[index][0];
+const index = process.argv[2] || 2;
+const target = cases[index-2][0];
 visualize(target,index,true);
