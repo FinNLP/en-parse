f1aefe47e5dafb5785486737ace3b54e6001fc14
Alex Corvi <alex@arrayy.com>
2017-01-21 01:49:29 +0300

    added benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,24 @@
+const cases = require('./cases.js');
+const parser = require("../lib/index.js");
+const pos = require("en-pos");
+
+global.b = [];
+
+const results = {
+	factory:0,
+	chunking:0,
+	relating:0,
+	repairing:0
+};
+
+
+cases.forEach((sample)=>{
+	var tags = pos(sample[0]).tags;
+	var parsed = parser(tags,sample[0]);
+	results.factory = results.factory + global.b[1] - global.b[0];
+	results.chunking = results.chunking + global.b[2] - global.b[1];
+	results.relating = results.relating + global.b[3] - global.b[2];
+	results.repairing = results.factory + global.b[4] - global.b[3];
+});
+
+console.log(results);
