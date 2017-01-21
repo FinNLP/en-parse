b24c7362936ad60548892c28d8da1fca4d0fccb0
Alex Corvi <alex@arrayy.com>
2017-01-21 23:18:35 +0300

    added array creation to benchmark

--- test/benchmark.js
+++ test/benchmark.js
@@ -8,7 +8,8 @@ const results = {
 	factory:0,
 	chunking:0,
 	relating:0,
-	repairing:0
+	repairing:0,
+	toArr:0
 };
 
 
@@ -18,7 +19,8 @@ cases.forEach((sample)=>{
 	results.factory = results.factory + global.b[1] - global.b[0];
 	results.chunking = results.chunking + global.b[2] - global.b[1];
 	results.relating = results.relating + global.b[3] - global.b[2];
-	results.repairing = results.factory + global.b[4] - global.b[3];
+	results.repairing = results.repairing + global.b[4] - global.b[3];
+	results.toArr = results.toArr + global.b[5] - global.b[4];
 });
 
 console.log(results);
