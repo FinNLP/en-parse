488b9c1b649feaa2ad7189ab8dba1ab34e469e6a
Alex Corvi <alex@arrayy.com>
2017-03-01 15:43:21 +0300

    removed old stuff

--- test/generate_results.js
+++
@@ -1,8 +0,0 @@
-const cases = require('./cases.json');
-const parser = require("../lib/index.js");
-const pos = require("en-pos");
-const fs = require('fs');
-
-const results = [];
-cases.forEach((sample)=>results.push(parser(pos(sample).tags,sample)));
-fs.writeFileSync("test/results.json",JSON.stringify(results,null,4));
